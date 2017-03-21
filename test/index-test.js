f267431f312d25c7d0a03e63052562b9ce00fd1e
Jeff Katz <jeffreyerickatz@gmail.com>
2017-03-21 00:40:12 -0400

    adding test

--- test/index-test.js
+++ test/index-test.js
@@ -14,13 +14,21 @@ describe('#findMinAndRemove', function() {
     findMinAndRemove(array)
     expect(array.includes(2)).toEqual(false)
   });
-});
 
+
+});
 
 
 describe('#selectionSort', function() {
   it("should sort an unsorted array", function() {
     let unsorted = [12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]
     expect(selectionSort(unsorted)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+  });
+
+  it("should make the proper calls on findMinAndRemove", function() {
+    var findMinAndRemove = sinon.spy(window, "findMinAndRemove");
+    let unsorted = [12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]
+    expect(selectionSort(unsorted)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+    expect(findMinAndRemove.callCount).toEqual(16)
   });
 });
