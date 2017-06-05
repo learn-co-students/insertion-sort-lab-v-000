8004172e18492d780a0c0a92add0fe5e9caf953c
Keegan Leitz <kjleitz@gmail.com>
2017-06-05 18:32:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+  let i, j;
 
+  for (i = 1, j = 0; i < array.length; i++) {
+    if (array[i] < array[j]) j = i;
+  }
+
+  return array.splice(j, 1)[0];
 }
 
-function insertionSort(array){
+function insertionSort(array) {
+  const sorted = [];
+  let i, l;
+  
+  for (i = 0, l = array.length; i < l; i++) {
+    sorted.push(findMinAndRemove(array));
+  }
 
+  return sorted;
 }
