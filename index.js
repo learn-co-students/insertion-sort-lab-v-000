a3ed1a19f635854cb2669ed08aa8ea0177831b13
Yechiel K <chilikkal@gmail.com>
2017-06-05 20:17:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function findMinAndRemove(array){
-
+  let min = 0
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < array[min]) {
+      min = i
+    }
+  }
+  return array.splice(min, 1)[0]
 }
 
 function insertionSort(array){
-
+  let sortedArray = []
+  while (array.length > 0) {
+    sortedArray.push(findMinAndRemove(array))
+  }
+  return sortedArray
 }
