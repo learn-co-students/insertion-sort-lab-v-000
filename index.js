b958f0d24b02f3d7dfc275fe40972cc8949daa74
Enoch2k2 <enoch2k2@hotmail.com>
2017-06-14 08:36:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  var min = array[0];
+  var minIndex = 0;
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] < min){
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  var sortedArray = [];
+  while(array.length > 0){
+    var min = findMinAndRemove(array);
+    sortedArray.push(min);
+  }
+  return sortedArray;
 }
