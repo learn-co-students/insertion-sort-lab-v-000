c8edb48613e97c623bbe7eb6dd49237ae6220e04
SiamKing <thesiameseelectric@gmail.com>
2017-07-05 20:41:23 -0700

    sorted

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function findMinAndRemove(array){
+function findMinAndRemove(arr){
+  let min = arr[0],
+      minIndex = 0,
+      i;
 
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      minIndex = i;
+    }
+  }
+  arr.splice(minIndex, 1);
+  return min;
 }
 
-function insertionSort(array){
+function insertionSort(arr){
+  let sorted = [];
 
+  while (!!arr.length) {
+    sorted.push(findMinAndRemove(arr));
+  }
+  return sorted;
 }
