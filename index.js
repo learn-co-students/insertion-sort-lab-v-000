fc7f251de48cb6693efb0322c4afdad8f7658727
bentonwong <bentonwong@gmail.com>
2017-07-30 14:50:46 -0700

    done

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let minIndex = 0
+  for (let i=0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex,1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sortedArray=[];
+  while (array.length > 0){
+    sortedArray.push(findMinAndRemove(array))
+  }
+  return sortedArray;
 }
