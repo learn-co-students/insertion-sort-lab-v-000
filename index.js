376cc9ae52261abe6bbf65faadb7f7a3998d3116
Zenia <zeniavilla@hotmail.com>
2017-09-19 07:50:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
+  var min = array[0];
+  var minIndex = 0;
 
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  var sorted = [];
+  while (array.length > 0) {
+    sorted.push(findMinAndRemove(array));
+  }
+  return sorted;
 }
