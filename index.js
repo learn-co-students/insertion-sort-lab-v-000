aeb416a339659ab017f10740d136842fa9f5b425
tylertaylor <vikingtyty@gmail.com>
2017-10-03 11:55:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
+    // Start with first element of array
+    let min = array[0]
 
+    let minIndex = 0
+
+    for(let i = 0; i < array.length; i++){
+        let currentElement = array[i]
+        if(array[i] < min){
+            min = array[i]
+            minIndex = i
+        }
+    }
+
+    array.splice(minIndex, 1);
+    return min
 }
 
 function insertionSort(array){
-
+    let newMin;
+    let sorted = [];
+    while(array.length !=0){
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted;
 }
