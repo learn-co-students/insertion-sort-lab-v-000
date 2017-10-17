03bf26116aa6bfc4c10926edf42e5d260f15b45c
tpstar <hanlee12@gmail.com>
2017-10-16 20:50:30 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  var min = array[0]
+  var minIndex = 0;
+  for (let i=1; i<array.length; i++) {
+    if (min > array[i]) {
+      minIndex = i;
+      min = array[i]
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
+  var newArray = [];
+  var len = array.length;
+  for (let i=0; i<len;i++) {
+    newArray.push(findMinAndRemove(array));
+  }
+  return newArray;
 
 }
