147b76c70e5b577fe2f9bdf90e30987d800bde9a
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-28 20:47:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let smallest = array[0]
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < smallest) {
+      smallest = array[i];
+    }
+  }
+  let index = array.indexOf(smallest)
+  return array.splice(index,1)
 }
 
 function insertionSort(array){
-
+    let newMin;
+    let sorted = []
+    while(array.length != 0){
+      newMin = findMinAndRemove(array)
+      sorted.push(newMin[0])
+    }
+    return sorted;
 }
