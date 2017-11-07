03a4b24d58d75deacc2181759445760f26a11143
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-07 15:14:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0]
+  let minIndex = 0
+  for(let i = 0; i < array.length; i++){
+    let currentElement = array[i]
+    if(array[i] < min){
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  while(array.length != 0){
+    let min = findMinAndRemove(array)
+      sorted.push(min)
+    }
+  return sorted;
 }
