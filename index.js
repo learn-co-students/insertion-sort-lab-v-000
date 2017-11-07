60cb5a2bef8e621425628d5906144d65de848121
alecgalba <alecgalba@gmail.com>
2017-11-07 17:25:10 -0500

    sorting has neve been so confusing

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+  let minimum = array[0];
+  let minimumIndex = 0;
+  let i = 0;
 
+  for(i=0;i<array.length;i++){
+    if(array[i] < minimum){
+      minimum = array[i];
+      minimumIndex = i;
+    }
+  }
+  array.splice(minimumIndex, 1);
+  return minimum;
 }
 
 function insertionSort(array){
+  let sortedArray = [];
 
+  while(!!array.length){
+    sortedArray.push(findMinAndRemove(array));
+  }
+  return sortedArray;
 }
