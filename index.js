31117ba5333add5f6ab1e9c2129e7250eec32eaf
dustin <dandersonsoftware@outlook.com>
2018-01-11 13:06:26 -0500

    first function of insertion sort

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
-
+  let minValue = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i]
+      minIndex = i
+    }
+  }
+  return array.splice(minIndex, 1)
 }
 
 function insertionSort(array){
