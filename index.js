74e1dddef46769da88429f3724e3e650ec29a41e
f3mshep <superbiscuit@gmail.com>
2018-01-22 13:01:34 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(myNumbers) {
+  let smallest = myNumbers[0];
+  for (const myNumber of myNumbers) {
+    if (myNumber < smallest) {
+      smallest = myNumber;
+    }
+  }
+  for (let index = 0; index < myNumbers.length; index++) {
+    if (myNumbers[index] === smallest) {
+      myNumbers.splice(index, 1);
+    }
+  }
+  //Big N is N so far...
+  return smallest;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let newArray = [];
+  let counter = 0;
+  const limit = array.length;
+  while (counter < limit) {
+    const smallBoi = findMinAndRemove(array);
+    newArray.push(smallBoi);
+    counter++;
+  }
+  return newArray;
 }
