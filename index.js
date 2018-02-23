07fd5076e6309b741e5031a1ce9582b9917f3e2f
Adam Tropp <adt6261@gmail.com>
2018-02-23 18:45:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIndex = 0;
+  for (let i=0; i<array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted = [];
+  while (array.length > 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
