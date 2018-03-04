d9ec408d370ede166c5da315a932351e789927b5
Moshe Levi <mo6709@gmail.com>
2018-03-04 14:16:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 function findMinAndRemove(array){
-
+    let min = array[0]; 
+    let minIndex = 0;
+    for(let i = 0; i < array.length; i++){
+        let currentItem = array[i];
+        if(currentItem < min){
+        	min = currentItem;
+        	minIndex = i; 
+        }   
+    }
+    array.splice(minIndex, 1);
+    return min;
 }
 
 function insertionSort(array){
-
+	let sortedArray = [];
+	// function pushMin(array){
+	// 	if(array.length < 2){
+	// 		sortedArray.push(array[0])
+	// 	}else {
+	// 		let min = findMinAndRemove(array)
+	// 		sortedArray.push(min)
+	// 		pushMin(array);
+	// 	}
+	// }
+	// pushMin(array);
+	while(array.length !== 0){
+		let min = findMinAndRemove(array)
+		sortedArray.push(min)
+	}
+	return sortedArray;
 }
