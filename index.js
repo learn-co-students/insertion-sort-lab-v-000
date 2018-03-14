478be0a72bde38983e87b7c78123f19e99156e5d
jamesonbass1987 <jamesonbass@gmail.com>
2018-03-14 16:55:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let index = 0;
 
-}
+    for (let i = 0; i < array.length; i++){
+        if (array[i] < min){
+            min = array[i];
+            index = i;
+        };
+    };
+
+    array.splice(index, 1);
+    return min;
+};
 
 function insertionSort(array){
+    const arrayCopy = [];
+    while (array.length > 0){
+        arrayCopy.push(findMinAndRemove(array));
+    };
 
+    return arrayCopy;
 }
