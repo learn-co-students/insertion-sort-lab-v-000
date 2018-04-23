4b14b02b84f75869cf017977d3b3c083c23fc4e7
ruuuh20 <pkim2320@gmail.com>
2018-04-23 00:26:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
+  let minNum = array[0]
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i]
+    if (array[i] < minNum) {
+      minNum = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1)
+  return minNum;
+  // returns the number and keeps the array spliced, without the number
 
 }
 
 function insertionSort(array){
+    let sorted = [];
+    let newMin;
+    while (array != 0) {
+      newMin = findMinAndRemove(array);
+      sorted.push(newMin)
+    }
+    return sorted;
 
 }
