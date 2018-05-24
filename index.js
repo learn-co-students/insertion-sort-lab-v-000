2293db852bd49abcb249ca0960f4fd6f666a6f91
David Towster <djt4xe@virginia.edu>
2018-05-23 22:14:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+  let min = array[0];
+  let minIndex = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
+  let sortedArray = [];
+  let length = array.length
 
+  for (let i = 0; i < length; i++) {
+    sortedArray.push(findMinAndRemove(array));
+  }
+
+  return sortedArray;
 }
