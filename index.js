7f7c9a5163fb50e7b3604408cfd06f06a37b15e2
cdhenry <cdhenry@gmail.com>
2018-06-20 19:10:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIdx = 0;
+  array.forEach((element, idx) => {
+    if (min > element){
+      min = element;
+      minIdx = idx;
+    }
+  })
+  array.splice(minIdx, 1)
+  return min;
 }
 
 function insertionSort(array){
-
+  let sortedArray = [];
+  while(array.length > 0){
+    sortedArray.push(findMinAndRemove(array))
+  }
+  return sortedArray;
 }
