580271fc437551f2c5f7a701d59953ec90d0de8c
Allyson Wesman <allysonwesman@gmail.com>
2018-06-27 12:00:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+  let minIndex = 0;
+  let currentMin = array[0]
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < currentMin) {
+      currentMin = array[i];
+      minIndex = i;
+    }
+  }
+
+  array.splice(minIndex, 1)
+  return currentMin;
 }
 
 function insertionSort(array){
+  let sorted = [];
 
+  while (array.length > 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+
+  return sorted;
 }
