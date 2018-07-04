4b4289a296ede9940a82a71e3c1bf8043f108427
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-07-03 21:27:57 -0400

    Implement insertion sort. Browserify test suite.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+    let minIndex = 0
+    let min = array[minIndex]
 
+    for (const [index, value] of array.entries()) {
+        if (value < min) {
+            min = value
+            minIndex = index
+        }
+    }
+
+    return (array.splice(minIndex, 1))[0]
 }
 
 function insertionSort(array){
+    let sorted = []
+    
+    while (0 != array.length){
+        sorted.push(findMinAndRemove(array))
+    }
 
+    return sorted
 }
