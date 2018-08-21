9c64465e15f8eb3e564f7db61c8911a9290a455e
Heather You <heatherhyou@gmail.com>
2018-08-21 13:31:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let newIndex = 0;
 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            newIndex = i;
+        }
+    }
+    array.splice(newIndex, 1);
+    return min;
 }
 
 function insertionSort(array){
+    let sortedArray = [];
+    let newMin;
 
+    while (array.length > 0) {
+        newMin = findMinAndRemove(array)
+        sortedArray.push(newMin);
+    }
+
+    return sortedArray;
 }
