664c51bf963f33550caabf9eec55f0d84c3331a8
Greg B <greg.benjamin23@gmail.com>
2018-08-29 17:17:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
-
+  let min
+  let minIndex
+  for (let index = 0; index < array.length; index++) {
+    if (index === 0){
+      minIndex = index
+      min = array[index];
+    } else {
+      if (array[index] < min){
+        min = array[index]
+        minIndex = index
+      }
+    }
+  }
+  return array.splice(minIndex, 1);
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  while (array.length != 0) {
+    const min = findMinAndRemove(array);
+    sorted.push(min[0]);
+  }
+  return sorted;
 }
