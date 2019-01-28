e59fe8d278f48ccfbb3e3420a5d5e8975bed0c4e
alyssa0528 <alyssa.kim@gmail.com>
2019-01-28 13:06:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let minValue = array[0];
+    let minIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i];
+            minIndex = i;
+        }
+    }
+    array.splice(minIndex, 1)  //updates array with minValue removed
+    return minValue;
 }
 
-function insertionSort(array){
-
+function insertionSort(array){ //[12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]
+    let sorted = [];
+    let min;
+    while (array.length > 0) {
+        min = findMinAndRemove(array) //returns minValue
+        sorted.push(min)
+    }
+    return sorted;
 }
