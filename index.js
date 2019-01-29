7ceec11516fcdd07c3e2fb378f785f365044109c
anikonchuk <anikonch@gmail.com>
2019-01-29 10:03:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i];
+    if (currentElement < min) {
+      min = currentElement;
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  let sorted = [];
+  let newMin;
+  while(array.length != 0) {
+    newMin = findMinAndRemove(array);
+    sorted.push(newMin)
+  }
+  return sorted;
 }
