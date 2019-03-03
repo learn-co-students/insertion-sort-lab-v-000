3960c309945b0cacdbe0b14d30e1bac38d2eacc0
Tammy Metz <tammy@tammymetz.com>
2019-03-03 03:51:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  let tempArray = [];
+  while(array.length > 0) {
+    tempArray.push(findMinAndRemove(array));
+  }
+  return tempArray;
 }
