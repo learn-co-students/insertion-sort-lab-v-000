768b5b504611fcb65814dafb1fccfe84c2dd49f8
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-03-15 17:42:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let minimum = array[0]
+  let minIndex = 0
+  for (let i=0; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return minimum
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  while (array.length !== 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
