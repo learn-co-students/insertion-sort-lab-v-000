e7f034c72ac1aa468b572442e0b5084784792f87
btmccollum <btmcco8@gmail.com>
2019-03-21 16:33:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+    // declare a minval to store the smallest number
+    let minVal = array[0]
+    let minIndex = 0
 
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] < minVal) {
+            minVal = array[i]
+            minIndex = i
+        }
+    }
+
+    array.splice(minIndex, 1)
+    return minVal
 }
 
 function insertionSort(array){
+    let sortedArr = []
+    let minVal = array[0]
 
+    while (array.length != 0) {
+        sortedArr.push(findMinAndRemove(array))
+    }
+    return sortedArr
 }
