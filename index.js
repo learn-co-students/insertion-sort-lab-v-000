1a3d2bc79f7f78f919e9d94e842016c38a43121d
Jack Yeung <jyeung.se@gmail.com>
2019-03-28 23:45:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i]
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  let min
+  while (array.length != 0) {
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted
 }
