3eb643a2af9cbdb34144dcb18618e1b841fe0278
Jenny Kam <jkam17@gmail.com>
2019-04-09 14:02:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let ind
+  let min = array[0]
+  for (let i = 0; i<array.length; i++){
+    if (array[i] < min){
+      min = array[i]
+      ind = i
+    }
+  }
+  array.splice(ind,1)
+  return min
 }
 
 function insertionSort(array){
-
+  // let len = array.length
+  let sorted=[]
+  while (array.length > 0){
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
