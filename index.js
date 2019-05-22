672fefed9406237566683c018b2c86bb9edfdd34
“vasu” <nvseejoor@gmail.com>
2019-05-22 17:02:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let arrMin = array[0];
+  let arrIdx = 0;
+  array.forEach((el, idx) => {
+    if (arrMin > el) {
+      arrMin = el;
+      arrIdx = idx;
+    }
+  });
+  const num = array.splice(arrIdx, 1);
+  return num[0];
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sorted = [];
+  while (array.length != 0) {
+    let newMin = findMinAndRemove(array);
+    sorted.push(newMin);
+  }
+  return sorted;
 }
