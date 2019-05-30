2fc99f0eb46e122ec6989e9c388013650d08b23b
blackbeltdiaries <github email address>
2019-05-30 10:27:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array){                   // [2, 3, 4, 5, 6, 7]
+  let min = array[0]                                // 2
+  let minIndex = 0
 
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i]
+
+    if (array[i] < min) {                           // compares min against all elements in array until either it is the biggest or another index is
+      min = array[i]                                // becomes new minimum if there is an element that is smaller
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1);
+
+  return min;
 }
 
 function insertionSort(array){
+  let newMin;
+  let sorted = []
 
+  while (array.length != 0) {
+    newMin = findMinAndRemove(array)
+    sorted.push(newMin)
+  }
+
+  return sorted;
 }
