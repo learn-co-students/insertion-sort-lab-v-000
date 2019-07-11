9ff11031737a831df6906234fc35f1e48ec85ec8
nichia <nichia@gmail.com>
2019-07-11 17:30:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] < min) {
+      min = array[index];
+      minIndex = index;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let newMin;
+  let sortedArray = [];
+  while (array.length > 0) {
+    newMin = findMinAndRemove(array);
+    sortedArray.push(newMin);
+  }
+  return sortedArray;
 }
