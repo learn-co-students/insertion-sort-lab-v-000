dfacef9353bf471de01a73aab00ef04958dd8d5b
Lois Lee <lois.lee@me.com>
2019-08-31 22:43:51 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function findMinAndRemove(array){
   let min = array[0]
   let minIndex = 0
   for(let i = 0; i < array.length; i++){
-    let currentElement = array[i]
     if(array[i] < min){
       min = array[i]
       minIndex = i
@@ -13,11 +12,9 @@ function findMinAndRemove(array){
 }
 
 function insertionSort(array){
-  let newMin;
   let sorted = []
-  while(array.length != 0){
-    newMin = findMinAndRemove(array)
-    sorted.push(newMin)
+  while(array.length > 0){
+    sorted.push(findMinAndRemove(array))
   }
 return sorted;
 }
