f4573c937ebb933230ef54d0ad6d7f6595108d5b
Travis Cole <iamtraviscole@gmail.com>
2019-09-24 23:14:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(arr){
+  let min = arr[0]
+  let minIndex = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]
+      minIndex = i
+    }
+  }
+  arr.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  while (array.length) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
