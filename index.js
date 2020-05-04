c6b15e9e2640323f056758778de516f2457a3bdb
ahotchkin <allyson.hotchkin@gmail.com>
2020-05-04 17:52:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    };
+  };
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
-
+  let min;
+  let sortedArray = [];
+  while (array.length > 0) {
+    min = findMinAndRemove(array);
+    sortedArray.push(min);
+  }
+  return sortedArray;
 }
