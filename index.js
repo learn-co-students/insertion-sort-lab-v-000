ed7402aaa454a53004c58decc27c9f316b61c062
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-12-18 11:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function findMinAndRemove(array){
-
+  let minVal;
+  for (let i; i < array.length; i++) {
+    if (array[i] < array[i++]) {
+      minVal = array[i];
+    }
+  }
+  return minVal;
 }
 
 function selectionSort(array){
