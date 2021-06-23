b50dd9ad6dbc43af6ae9317bbb44e149d0d5f955
Matthew Williams <matthew.williams@flatironschool.com>
2021-06-22 22:57:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 function findMinAndRemove(array){
 
+    let min = array[0]
+    let min_index = 0
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < min){
+            min = array[i]
+            min_index = i
+        }
+    }
+
+    array.splice(min_index, 1);
+
+    // debugger
+    return min
 }
 
 function selectionSort(array){
+    
+    let sorted = []
+    let min
+
+    while(array.length > 0){
+        
+        min = findMinAndRemove(array)
+        sorted.push(min)
+
+    }
+
+    return sorted
 
 }
+// function findMinAndRemove(array){
+//     let currentMin = array[0]
+//     let minIndex = 0
+//     for(let i = 0; i < array.length; i++){
+//       if(array[i] < currentMin){
+//         currentMin = array[i]
+//         minIndex = i
+//       }
+//     }
+//     array.splice(minIndex, 1);
+//     return currentMin;
+//   }
+  
+//   function selectionSort(array){
+//     let sorted = []
+//     let min;
+//     while(array.length != 0){
+//       min = findMinAndRemove(array)
+//       sorted.push(min)
+//     }
+//     return sorted;
+// }
